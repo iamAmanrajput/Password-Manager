5158bbfc57dcea768c44cbd836385b5ae9c47892
Aman Singh <aman.it360@gmail.com>
2024-10-15 14:24:42 +0530

    Added Project Files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Password-Manager
+
 A secure and user-friendly password manager designed to help users safely store and manage Site name , Username and passwords. This app employs advanced encryption techniques to ensure that sensitive information is protected while remaining easily accessible.
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-slate-800 text-white ">
+      <div className="mycontainer flex justify-between items-center px-4 py-5 h-14">
+        <div className="logo font-bold text-white text-2xl">
+          <span className="text-green-500"> &lt;</span>
+
+          <span>Pass</span>
+          <span className="text-green-500">OP&gt;</span>
+        </div>
+
+        <a
+          href="https://github.com/iamAmanrajput/Password-Manager"
+          target="_blank"
+          className="text-white  my-5 mx-2  flex  justify-between items-center"
+        >
+          <img
+            className="invert  w-10 p-1"
+            src="/icons/github.svg"
+            alt="github logo"
+          />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
